f04be78297c4022e14845b649165620b749c4b9c
Karolis Davli <kdavlijevas@gmail.com>
2022-02-21 17:49:03 +0100

    remove button on book card

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-const libraryView = document.querySelector('#library-view');
-const openBookButton = document.querySelector('#open-form-button');
+const bookshelf = document.querySelector('#bookshelf');
+const libraryView = document.querySelector('#library-view')
+const openFormButton = document.querySelector('#open-form-button');
 const bookForm = document.querySelector('#book-form');
     
-let library = [{
-  title: 'HaPpy potter',
-  author: 'Rawling',
-  pages: 345,
-  read: true
-}, {
-  title: 'Yo',
-  author: 'What is this book',
-  pages: 5,
-  read: false
-}];
-
-
-
+let library = [];
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -35,29 +23,73 @@ const valdovas = new Book('LOTR', 'Talkien', 320, true)
 function renderBooks() {
   library.forEach((singleBook) => {
     let bookCard = document.createElement('div');
+    bookCard.classList.add(`${library.length}`);
     let bookTitle = document.createElement('h5');
     let bookAuthor = document.createElement('h6');
     let bookPages = document.createElement('p');
     let bookRead = document.createElement('p')
   
-    bookTitle.innerText = singleBook.title;
-    bookAuthor.innerText = singleBook.author;
-    bookPages.innerText = singleBook.pages;
-    bookRead.innerText = singleBook.read;
+    bookTitle.textContent = singleBook.title;
+    bookAuthor.textContent = singleBook.author;
+    bookPages.textContent = singleBook.pages;
+    bookRead.textContent = singleBook.read;
   
   
     bookCard.classList.add('book-card');
-  
-    bookCard.append(bookTitle, bookAuthor, bookPages, bookRead);
-    libraryView.append(bookCard);
+
+    let remove = renderRemove();
+
+
+    bookCard.append(bookTitle, bookAuthor, bookPages, bookRead, remove);
+    bookshelf.append(bookCard);
+    libraryView.append(bookshelf);
   })
 }
 
-// Open new book form
-openBookButton.addEventListener('click', () => {
+
+// Generate remove button
+function renderRemove() {
+  const removeButton = document.createElement('button');
+  removeButton.addEventListener('click', removeBook)
+  removeButton.textContent = 'Remove';
+  return removeButton;
+}
+
+// Remove button event
+function removeBook() {
+  console.log('remove clicked');
+  console.log(this);
+}
+
+
+// Toggle book form
+ function checkIfOpen() {
   if (bookForm.style.display === 'block') {
     bookForm.style.display = 'none';
   } else {
     bookForm.style.display = 'block';
   }
+}
+
+// Open new book form
+openFormButton.addEventListener('click', checkIfOpen)
+
+
+// Submit book
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let authorInput = document.querySelector('#author').value;
+  let titleInput = document.querySelector('#title').value;
+  let pagesInput = document.querySelector('#pages').value;
+  let readInput = document.querySelector('#read').checked;
+
+  let newBook = new Book(authorInput, titleInput, pagesInput, readInput);
+  
+  renderBooks();
+  checkIfOpen();
+  
+  library.push(newBook);
+  console.log(library);
 })
+
