ddd23659150f86fbc9979cd0723d6fe2a0b29371
Karolis Davli <kdavlijevas@gmail.com>
2022-02-22 08:29:06 +0100

    rendering fix

--- script.js
+++ script.js
@@ -15,17 +15,33 @@ function Book(title, author, pages, read) {
   }
 }
 
-const valdovas = new Book('LOTR', 'Talkien', 320, true)
 
+function addBookToLibrary(e) {
+  e.preventDefault()
+  
+  let titleInput = document.querySelector('#title').value;
+  let authorInput = document.querySelector('#author').value;
+  let pagesInput = document.querySelector('#pages').value;
+  let readInput = document.querySelector('#read').checked;
 
+  // Construct new book and push it to an array
+  let newBook = new Book(titleInput, authorInput, pagesInput, readInput);
+  library.push(newBook);
+
+  // Clear html content before looping through array
+  bookshelf.textContent = '';
+  renderBooks();
+  checkIfOpen();
+}
 
 // Render library array to page
 function renderBooks() {
   library.forEach((singleBook) => {
     let bookCard = document.createElement('div');
     bookCard.classList.add(`${library.length}`);
-    let bookTitle = document.createElement('h5');
-    let bookAuthor = document.createElement('h6');
+
+    let bookTitle = document.createElement('h1');
+    let bookAuthor = document.createElement('h2');
     let bookPages = document.createElement('p');
     let bookRead = document.createElement('p')
   
@@ -37,10 +53,11 @@ function renderBooks() {
   
     bookCard.classList.add('book-card');
 
-    let remove = renderRemove();
+    let removeBtn = renderRemove();
 
+    console.log(library);
 
-    bookCard.append(bookTitle, bookAuthor, bookPages, bookRead, remove);
+    bookCard.append(bookTitle, bookAuthor, bookPages, bookRead, removeBtn);
     bookshelf.append(bookCard);
     libraryView.append(bookshelf);
   })
@@ -56,9 +73,10 @@ function renderRemove() {
 }
 
 // Remove button event
-function removeBook() {
+function removeBook(e) {
   console.log('remove clicked');
-  console.log(this);
+  console.log(e);
+
 }
 
 
@@ -76,20 +94,19 @@ openFormButton.addEventListener('click', checkIfOpen)
 
 
 // Submit book
-bookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+bookForm.addEventListener('submit', addBookToLibrary)
 
-  let authorInput = document.querySelector('#author').value;
-  let titleInput = document.querySelector('#title').value;
-  let pagesInput = document.querySelector('#pages').value;
-  let readInput = document.querySelector('#read').checked;
+// e.preventDefault();
 
-  let newBook = new Book(authorInput, titleInput, pagesInput, readInput);
-  
-  renderBooks();
-  checkIfOpen();
-  
-  library.push(newBook);
-  console.log(library);
-})
+// let authorInput = document.querySelector('#author').value;
+// let titleInput = document.querySelector('#title').value;
+// let pagesInput = document.querySelector('#pages').value;
+// let readInput = document.querySelector('#read').checked;
 
+// let newBook = new Book(authorInput, titleInput, pagesInput, readInput);
+
+// renderBooks();
+// checkIfOpen();
+
+// library.push(newBook);
+// console.log(library);
